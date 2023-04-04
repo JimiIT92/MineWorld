023267ad28b28b7406b2c3669920274c6d4d5050
JimiIT92 <jimiit92@gmail.com>
2023-04-04 02:19:01 +0200

    Partially implemented ruby trim

--- src/main/java/org/mineworld/core/MWItems.java
+++ src/main/java/org/mineworld/core/MWItems.java
@@ -1,18 +1,24 @@
 package org.mineworld.core;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.Util;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.flag.FeatureFlag;
-import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SmithingTemplateItem;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.Nullable;
 import org.mineworld.MineWorld;
-import org.mineworld.item.MWFuelItem;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 /**
@@ -40,9 +46,44 @@ public final class MWItems {
     public static final RegistryObject<Item> BRONZE_INGOT = registerSimpleItem("bronze_ingot");
     public static final RegistryObject<Item> BRONZE_NUGGET = registerSimpleItem("bronze_nugget");
     public static final RegistryObject<Item> COPPER_NUGGET = registerSimpleItem("copper_nugget");
-    public static final RegistryObject<Item> RAW_BRONZE_SMITHING_TEMPLATE = registerSimpleItem("raw_bronze_smithing_template", FeatureFlags.UPDATE_1_20);
+    public static final RegistryObject<Item> RAW_BRONZE_SMITHING_TEMPLATE = registerItem("raw_bronze_smithing_template",
+            () -> createBasicSmithingTemplate("raw_bronze", "raw_copper", "raw_aluminum"));
 
     //#endregion
+
+    /**
+     * Create a basic {@link SmithingTemplateItem smithing template}
+     *
+     * @param name {@link String The smithing template name}
+     * @param baseSlotIcon {@link ResourceLocation Icon to show inside the base slot}
+     * @param additionSlotIcon {@link ResourceLocation Icon to show inside the additions slot}
+     * @return {@link SmithingTemplateItem The smithing template}
+     */
+    private static SmithingTemplateItem createBasicSmithingTemplate(final String name, final String baseSlotIcon, final String additionSlotIcon) {
+        return createBasicSmithingTemplate(name,
+                List.of(new ResourceLocation(MineWorld.MODID, "item/empty_slot_" + baseSlotIcon)),
+                List.of(new ResourceLocation(MineWorld.MODID, "item/empty_slot_" + additionSlotIcon)));
+    }
+
+    /**
+     * Create a basic {@link SmithingTemplateItem smithing template}
+     *
+     * @param name {@link String The smithing template name}
+     * @param baseSlotIcons {@link List<ResourceLocation> List of icons to show inside the base slot}
+     * @param additionSlotIcons {@link List<ResourceLocation> List of icons to show inside the additions slot}
+     * @return {@link SmithingTemplateItem The smithing template}
+     */
+    private static SmithingTemplateItem createBasicSmithingTemplate(final String name, final List<ResourceLocation> baseSlotIcons, final List<ResourceLocation> additionSlotIcons) {
+        return new SmithingTemplateItem(
+                Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MineWorld.MODID, "smithing_template." + name + ".applies_to"))).withStyle(ChatFormatting.BLUE),
+                Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MineWorld.MODID, "smithing_template." + name + ".ingredients"))).withStyle(ChatFormatting.BLUE),
+                Component.translatable(Util.makeDescriptionId("upgrade", new ResourceLocation(MineWorld.MODID, name))).withStyle(ChatFormatting.GRAY),
+                Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MineWorld.MODID, "smithing_template." + name + ".base_slot_description"))),
+                Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MineWorld.MODID, "smithing_template." + name + ".additions_slot_description"))),
+                baseSlotIcons,
+                additionSlotIcons
+        );
+    }
 
     /**
      * Get the basic {@link Item.Properties item properties}
@@ -70,14 +111,26 @@ public final class MWItems {
     }
 
     /**
-     * Register a {@link MWFuelItem fuel item}
+     * Register a {@link Item fuel item}
      *
      * @param name {@link String The item name}
      * @param burnTime {@link Integer The fuel burn time} in seconds
      * @return {@link RegistryObject<Item> The registered item}
      */
     private static RegistryObject<Item> registerFuel(final String name, final int burnTime) {
-        return registerItem(name, () -> new MWFuelItem(burnTime));
+        return registerItem(name, () -> new Item(basicProperties()){
+            /**
+             * Get the {@link ItemStack Item Stack} burn time in ticks
+             *
+             * @param itemStack {@link ItemStack The Item Stack} being used as fuel
+             * @param recipeType {@link RecipeType The recipe type}
+             * @return {@link Integer Burn time} in ticks
+             */
+            @Override
+            public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType) {
+                return burnTime * 20;
+            }
+        });
     }
 
     /**
