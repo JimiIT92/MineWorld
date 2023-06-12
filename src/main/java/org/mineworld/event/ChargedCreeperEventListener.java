085fafddff5e086ffcb3f3c91cff495c318e402d
Jimi <jimiit92@gmail.com>
2023-06-12 21:54:10 +0200

    Fixed dropped skulls count

--- src/main/java/org/mineworld/event/ChargedCreeperEventListener.java
+++ src/main/java/org/mineworld/event/ChargedCreeperEventListener.java
@@ -7,11 +7,12 @@ import net.minecraft.world.entity.monster.Drowned;
 import net.minecraft.world.entity.monster.Husk;
 import net.minecraft.world.entity.monster.Stray;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.RegistryObject;
 import org.mineworld.MineWorld;
 import org.mineworld.core.MWItems;
 import org.mineworld.helper.ItemHelper;
@@ -34,26 +35,32 @@ public final class ChargedCreeperEventListener {
         final Entity entity = event.getEntity();
         if(event.getSource().getEntity() instanceof Creeper creeper && creeper.isPowered() && !(entity instanceof Player)) {
             final Collection<ItemEntity> drops = event.getDrops();
-            final Level level = entity.level;
             final Vec3 pos = entity.position();
             if(entity instanceof Husk) {
-                final ItemEntity huskHead = new ItemEntity(level, pos.x, pos.y, pos.z, ItemHelper.getDefaultStack(MWItems.HUSK_HEAD));
-                if(!drops.contains(huskHead)) {
-                    drops.add(huskHead);
-                }
+                tryDropSkull(creeper, pos, MWItems.HUSK_HEAD, drops);
             }
             else if(entity instanceof Drowned) {
-                final ItemEntity drownedHead = new ItemEntity(level, pos.x, pos.y, pos.z, ItemHelper.getDefaultStack(MWItems.DROWNED_HEAD));
-                if(!drops.contains(drownedHead)) {
-                    drops.add(drownedHead);
-                }
+                tryDropSkull(creeper, pos, MWItems.DROWNED_HEAD, drops);
             }
             else if(entity instanceof Stray) {
-                final ItemEntity straySkull = new ItemEntity(level, pos.x, pos.y, pos.z, ItemHelper.getDefaultStack(MWItems.STRAY_SKULL));
-                if(!drops.contains(straySkull)) {
-                    drops.add(straySkull);
-                }
+                tryDropSkull(creeper, pos, MWItems.STRAY_SKULL, drops);
             }
+        }
+    }
+
+    /**
+     * Try to drop a mob skull
+     *
+     * @param creeper {@link Creeper The creeper that killed the mob}
+     * @param pos {@link Vec3 The entity position}
+     * @param skullSupplier {@link RegistryObject The skull supplier}
+     * @param drops {@link Collection<ItemEntity> The entity drops}
+     */
+    private static void tryDropSkull(final Creeper creeper, final Vec3 pos, final RegistryObject<? extends ItemLike> skullSupplier, Collection<ItemEntity> drops) {
+        final ItemEntity skull = new ItemEntity(creeper.level, pos.x, pos.y, pos.z, ItemHelper.getDefaultStack(skullSupplier));
+        if(!drops.contains(skull) && creeper.canDropMobsSkull()) {
+            drops.add(skull);
+            creeper.increaseDroppedSkulls();
         }
     }
 
