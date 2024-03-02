a2e7735853ad7a444712b3a534877253bed08aee
Jimi <jimiit92@gmail.com>
2024-03-02 15:48:57 +0100

    Implemented MissingMappingsEvent handler

--- src/main/java/org/mineworld/event/MissingMappingEvents.java
+++ src/main/java/org/mineworld/event/MissingMappingEvents.java
@@ -1,10 +1,15 @@
 package org.mineworld.event;
 
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.MissingMappingsEvent;
 import org.mineworld.MineWorld;
 import org.mineworld.helper.ResourceHelper;
+
+import java.util.List;
 
 /**
  * Handle events for missing mappings
@@ -19,8 +24,43 @@ public final class MissingMappingEvents {
      */
     @SubscribeEvent
     public static void onMissingMapping(final MissingMappingsEvent event) {
-        if(event.getKey().location().equals(ResourceHelper.resourceLocation("volcanic_peak"))) {
-            System.out.println("MISSING");
-        }
+
     }
+
+    /**
+     * Get all {@link MissingMappingsEvent.Mapping<T> Missing  Mappings } for a {@link IForgeRegistry<T> Registry}
+     *
+     * @param event The {@link MissingMappingsEvent Missing  Mappings  Event}
+     * @param registry The {@link IForgeRegistry<T> Registry}
+     * @return The {@link MissingMappingsEvent.Mapping<T> Missing Mappings}
+     * @param <T> The Missing Mappings Type
+     */
+    private static <T> List<MissingMappingsEvent.Mapping<T>> getMappings(final MissingMappingsEvent event, final IForgeRegistry<T> registry) {
+        return getMappings(event, registry.getRegistryKey());
+    }
+
+    /**
+     * Get all {@link MissingMappingsEvent.Mapping<T> Missing  Mappings } for a {@link ResourceKey<Registry> Registry Key}
+     *
+     * @param event The {@link MissingMappingsEvent Missing  Mappings  Event}
+     * @param registryKey The {@link ResourceKey<Registry> Registry Key}
+     * @return The {@link MissingMappingsEvent.Mapping<T> Missing Mappings}
+     * @param <T> The Missing Mappings Type
+     */
+    private static <T> List<MissingMappingsEvent.Mapping<T>> getMappings(final MissingMappingsEvent event, final ResourceKey<? extends Registry<T>> registryKey) {
+        return event.getMappings(registryKey, MineWorld.MOD_ID);
+    }
+
+    /**
+     * Check if the provided {@link MissingMappingsEvent.Mapping<T> Missing Mapping} corresponds to the provided {@link String entry name}
+     *
+     * @param mapping The {@link MissingMappingsEvent.Mapping<T> Missing Mapping}
+     * @param name The {@link String entry name}
+     * @return {@link Boolean True if the Missing Mapping is for the provided entry name}
+     * @param <T> The Missing Mapping Type
+     */
+    private static <T> boolean checkMapping(final MissingMappingsEvent.Mapping<T> mapping, final String name) {
+        return ResourceHelper.path(mapping.getKey()).equals(ResourceHelper.lower(name));
+    }
+
 }
